e17b71f3495d362669daa54ae07a99253eca1053
Oli Griffiths <oli.griffiths@me.com>
2012-05-17 10:05:57 +0100

    Updated sql deployment file

--- trunk/db.sql
+++ trunk/db.sql
@@ -19,13 +19,16 @@ CREATE TABLE `jos_tokens` (
   UNIQUE KEY `key` (`key`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
-/** 1.7 Plugin **/
+/** 2.5 Install **/
 INSERT INTO `jos_extensions` (`extension_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `system_data`, `checked_out`, `checked_out_time`, `ordering`, `state`)
 VALUES
 	(0, 'System - Nooku API', 'plugin', 'koowa_tokens', 'system', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 2, 0);
 	
 	
-/*** 1.5 Plugin **/
+/*** 1.5 Install **/
+INSERT INTO `jos_components` (`id`, `name`, `link`, `menuid`, `parent`, `admin_menu_link`, `admin_menu_alt`, `option`, `ordering`, `admin_menu_img`, `iscore`, `params`, `enabled`)
+VALUES
+	(0, 'API Tokens', 'option=com_tokens', 0, 0, 'option=com_tokens', 'tokens', 'com_tokens', 0, '', 0, '', 1);
 INSERT INTO `jos_plugins` (`id`, `name`, `element`, `folder`, `access`, `ordering`, `published`, `iscore`, `client_id`, `checked_out`, `checked_out_time`, `params`)
 VALUES
 	(0, 'System - Nooku API', 'koowa_tokens', 'system', 0, 0, 1, 0, 0, 0, '0000-00-00 00:00:00', '');
